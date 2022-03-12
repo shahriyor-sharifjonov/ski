24829e148142206ad464eda389fa23c790f98ad7
Shahriyor - Keypix <78787657+shahriyor-sharifjonov@users.noreply.github.com>
2022-03-13 00:39:45 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -234,3 +234,21 @@ let reviews = new Swiper(".reviews__carousel", {
     },
   },
 });
+
+$(document).ready(function() {
+  $(".accordion > .accordion__button").on("click", function() {
+    if ($(this).hasClass("active")) {
+      $(this).removeClass("active");
+      $(this)
+        .siblings(".accordion__content")
+        .slideUp(200);
+    } else {
+      $(".accordion > .accordion__button").removeClass("active");
+      $(this).addClass("active");
+      $(".accordion__content").slideUp(200);
+      $(this)
+        .siblings(".accordion__content")
+        .slideDown(200);
+    }
+  });
+});
